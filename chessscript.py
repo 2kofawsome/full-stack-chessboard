304f593f044b305c6103f3ec6d069a91d9dad46a
SamGunter <samgunter12@gmail.com>
2020-06-18 00:00:34 -0400

    Small flow reformatting

--- chessscript.py
+++ chessscript.py
@@ -4,64 +4,55 @@ import RPi.GPIO as gpio
 from mfrc522 import SimpleMFRC522
 import hardwarescripts
 
-def setdifficulty():
-    pass
-    # gives option for button to be clicked
+
+def tofen(fen):
+    grid = togrid(fen)
+    fen = ""
+
+    for n in grid:
+        for m in n:
+            if m == " ":
+                if fen[-1].isnumeric():
+                    fen = fen[:-1] + str(int(fen[-1]) + 1)
+                else:
+                    fen += "1"
+            else:
+                fen += m
+        fen += r"/"
+    fen = fen[:-1]
+
+    return fen
 
 
-def updateboard():
-    grid = getboard()
-    last = getfen()
+def togrid(fen): #FEN without information at the end, eg "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR"
+    grid = [[]]
+    i = 0
+    n = 0
+    for m in fen:
+        if m == r"/":
+            n += 1
+            grid.append([])
+        elif m.isnumeric():
+            for m in range(int(m)):
+                grid[n].append("")
+        else:
+            grid[n].append(m)
+        i += 1
+    return grid
 
-    for n in range(len(grid)):
-        for m in range(len(grid[n])):
-            if grid[n][m] == 1 and last[n][m] == " " or \
-                    grid[n][m] == 0 and last[n][m] != " ":
-                print(str(8 - n) + alphabet[m])
-
-    # figure out what board/move makes sense from hardware
-
-    ### At this point it is alegraic notation
-    example = ["e2", "e4"]
-
-
-    if not stockfish.is_move_correct("".join(example)): #special for castling, just king movement
-        # do somethign if move not valid
-        pass
-
-
-    fen = updatefen(example)
-    stockfish.set_fen_position(fen)
-
-    # algebraic to FIDE
-    # update saved PGN
-
-    # check best move (skip if players turn)
-    # send to hardware
-
-    # check evaluation
-    LEDbar.setvalue([1, 1, 1, 1, 0, 0, 1, 0, 1, 1])
-    time.sleep(1)
-    LEDbar.clear()
-
-
-def getboard(): # will be determined with hardware
-    return [[1, 1, 1, 1, 1, 1, 1, 1], [1, 1, 1, 1, 1, 1, 1, 1],
-            [0, 0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 0],
-            [0, 0, 0, 0, 1, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 0],
-            [1, 1, 1, 1, 0, 1, 1, 1], [1, 1, 1, 1, 1, 1, 1, 1]]
 
 def updatepgn(algebraic):
-    global PGN
     fen = stockfish.get_fen_position()
     fen = fen.split(" ")
 
+    saved = open(("../PGNs/" + date + "/Game" + str(round) + ".txt"), "a")
+
     move = ""
     if fen[1] == "w":
-        move = move + fen[5] + ". "
+        saved.write(fen[5] + ". ")
 
     if algebraic[0] == "O-O-O" or algebraic[0] == "O-O":
-        move = move + algebraic[0]
+        saved.write(algebraic[0])
 
     else:
         grid = togrid(fen[0])
@@ -69,7 +60,7 @@ def updatepgn(algebraic):
         next = grid[-int(algebraic[1][1])][alphabet.index(algebraic[1][0])]
 
         if last != "p" and last != "P":
-            move = move + last.upper()
+            saved.write(last.upper())
 
         possibilities = []
         for n in range(8):
@@ -88,22 +79,16 @@ def updatepgn(algebraic):
             elif algebraic[0][1] != n[1] and (add[0] == " " or add[0] == n[0]): #number
                 add = add[0] + algebraic[0][1]
         add = add.replace(" ", "")
-        move = move + add
+        saved.write(add)
 
 
         if next != "":
-            move = move + "x"
+            saved.write("x")
 
-        move = move + algebraic[1]
+        saved.write(algebraic[1])
 
-    move = move + " "
-
-    PGN["moves"] = PGN["moves"] + move
-
-    saved = open(("../PGNs/" + PGN["Date"] + "/game" + str(round) + ".txt"), "w")
-    saved.write(PGN["moves"])
+    saved.write(" ")
     saved.close()
-    return PGN
 
 
 def updatefen(algebraic):
@@ -224,43 +209,32 @@ def updatefen(algebraic):
     return fen
 
 
-def tofen(fen):
-    grid = togrid(fen)
-    fen = ""
+def updateboard():
+    # figure out what board/move makes sense from hardware
 
-    for n in grid:
-        for m in n:
-            if m == " ":
-                if fen[-1].isnumeric():
-                    fen = fen[:-1] + str(int(fen[-1]) + 1)
-                else:
-                    fen += "1"
-            else:
-                fen += m
-        fen += r"/"
-    fen = fen[:-1]
-
-    return fen
+    ### At this point it is alegraic notation
+    example = ["e2", "e4"]
 
 
-def togrid(fen): #FEN without information at the end, eg "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR"
-    grid = [[]]
-    i = 0
-    n = 0
-    for m in fen:
-        if m == r"/":
-            n += 1
-            grid.append([])
-        elif m.isnumeric():
-            for m in range(int(m)):
-                grid[n].append("")
-        else:
-            grid[n].append(m)
-        i += 1
-    return grid
+    if not stockfish.is_move_correct("".join(example)): #special for castling, just king movement
+        # do somethign if move not valid
+        pass
 
 
-def main():
+    fen = updatefen(example)
+    updatepgn(example)
+    stockfish.set_fen_position(fen)
+
+    # check best move (skip if players turn)
+    # send to hardware
+
+    # check evaluation
+    LEDbar.setvalue([1, 1, 1, 1, 0, 0, 1, 0, 1, 1])
+    time.sleep(1)
+    LEDbar.clear()
+
+
+def main(): #this should loop
     # Tracks 2 buttons
     button1 = False
     if button1 == True:
@@ -268,46 +242,49 @@ def main():
     # Tracks 3 switches
     # Tracks board/reed switch movements
 
+    # once player turn is determined
+    updateboard()
 
 
-    stockfish.set_fen_position("r3k2r/pppppppp/8/8/7b/8/PPPPPPPP/R3K2R w KQkq - 0 1")
+def startup():
 
-    example = ["O-O-O"]
-    print(updatefen(example))
-    print(updatepgn(example))
+    # output "scan your king"
+    # RFID.read()
 
-    input()
+    # set single or double
 
+    # choose difficulty
 
+    if "PGNs" not in os.listdir(".."):
+        os.makedirs("../PGNs")
+    if date not in os.listdir("../PGNs"):
+        os.makedirs("../PGNs/" + date)
+
+    saved = open(("../PGNs/" + date + "/Game" + str(round) + ".txt"), "w")
+    saved.write('[Event "Unknown"]\n[Site "Unknown"]\n[Date "')
+    saved.write(date)
+    saved.write('"]\n[Round "')
+    saved.write(str(round))
+    saved.write('"]\n[White "')
+    saved.write("Player") # Determined by above
+    saved.write('"]\n[Black "')
+    saved.write("AI - Level __") # Determined by above
+    saved.write('"]\n[Result "*"]\n\n')
+    saved.close()
+
+    stockfish.set_fen_position("rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1")
+
+    main()
 
 alphabet = list(string.ascii_lowercase)
 LEDbar = hardwarescripts.the74HC595()
 RFID = SimpleMFRC522()
 stockfish = Stockfish("/home/pi/full-stack-chessboard/stockfish", depth=8)
 
-PGN = {"Event":"Unknown", "Site":"Unknown", "Date":"", "Round":"",
-       "White":"", "Black":"", "Result":"", "moves":""}
-date =  datetime.datetime.now()
-PGN["Date"] = date.strftime("%Y.%m.%d")
-
-if "PGNs" not in os.listdir(".."):
-    os.makedirs("../PGNs")
-if PGN["Date"] not in os.listdir("../PGNs"):
-    os.makedirs("../PGNs/" + PGN["Date"])
+date =  datetime.datetime.now().strftime("%Y.%m.%d")
 round = 1
-for n in os.listdir("../PGNs/" + PGN["Date"]):
+for n in os.listdir("../PGNs/" + date):
     round += 1
-saved = open(("../PGNs/" + PGN["Date"] + "/game" + str(round) + ".txt"), "w")
-saved.write("")
-saved.close()
 
-PGN["Round"] = str(round)
-PGN["White"] = "Unknown" #AI or player
-PGN["Black"] = "Unknown" #AI or player
-PGN["Result"] = "*"
-
-stockfish.set_fen_position("rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1")
-
-while True:
-    main()
+startup()
 
